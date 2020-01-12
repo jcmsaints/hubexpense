3d88d2c7fc4de9085c8a82e92695fb3518a24974
jcmsaints <joao.c.matias.santos@gmail.com>
2020-01-12 18:38:37 +0000

    port de 3000 para 80

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 80;
 const passport = require("passport");
 const users = [];
 const flash = require("express-flash");
