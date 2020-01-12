d7848b135adadfe64d8ff9243c02e5f6d732d11c
jcmsaints <joao.c.matias.santos@gmail.com>
2020-01-12 09:12:04 +0000

    adicionar categoria

---
+++ app/public/js/main.js
@@ -0,0 +1,38 @@
+function addDatePicker(selector) {
+  $(selector).datepicker({
+    changeMonth: true,
+    changeYear: true,
+    dateFormat: "dd/mm/yy",
+    dayNamesMin: ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"],
+    monthNamesShort: [
+      "Jan",
+      "Fev",
+      "Mar",
+      "Abr",
+      "Mai",
+      "Jun",
+      "Jul",
+      "Ago",
+      "Set",
+      "Out",
+      "Nov",
+      "Dez"
+    ]
+  });
+  $(selector).datepicker("setDate", "-0d");
+}
+function addDataTable(selector) {
+  $(document).ready(function() {
+    $(selector).DataTable({
+      scrollY: "300px",
+      scrollCollapse: true,
+      paging: false,
+      language: {
+        search: "Pesquisa:",
+        infoEmpty: "Sem resultados",
+        info: "Total de _MAX_ Resultados",
+        infoFiltered: "(filtrado de _MAX_ total)"
+      }
+    });
+  });
+}
