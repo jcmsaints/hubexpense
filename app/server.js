4f99558f0582a319df02bc90f7af79bdbe7cf15a
jcmsaints <joao.c.matias.santos@gmail.com>
2020-01-12 18:39:38 +0000

    novamente 3000

--- app/server.js
+++ app/server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = 3000;
 const passport = require("passport");
 const users = [];
 const flash = require("express-flash");
